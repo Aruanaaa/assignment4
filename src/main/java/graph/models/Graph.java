0433c3f60dbe83b4db2f007adbe6571b53e1b12d
Aruana <ZHAKSYLYKARUANA@GMAIL.COM>
2025-10-31 23:34:56 +0500

    SCC result

--- src/main/java/graph/models/Graph.java
+++ src/main/java/graph/models/Graph.java
@@ -1,4 +1,29 @@
+// src/main/java/graph/models/Graph.java
 package graph.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Graph {
+    private List<Node> nodes;
+    private List<Edge> edges;
+
+    public Graph() {
+        this.nodes = new ArrayList<>();
+        this.edges = new ArrayList<>();
+    }
+
+    // Геттеры и сеттеры
+    public List<Node> getNodes() { return nodes; }
+    public void setNodes(List<Node> nodes) { this.nodes = nodes; }
+
+    public List<Edge> getEdges() { return edges; }
+    public void setEdges(List<Edge> edges) { this.edges = edges; }
+
+    public void addNode(Node node) { nodes.add(node); }
+    public void addEdge(Edge edge) { edges.add(edge); }
+
+    public Node getNodeById(String id) {
+        return nodes.stream().filter(n -> n.getId().equals(id)).findFirst().orElse(null);
+    }
 }
--- src/main/java/graph/models/Node.java
+++ src/main/java/graph/models/Node.java
@@ -1,4 +1,39 @@
+// src/main/java/graph/models/Node.java
 package graph.models;
 
 public class Node {
+    private String id;
+    private int duration;
+
+    public Node() {}
+
+    public Node(String id, int duration) {
+        this.id = id;
+        this.duration = duration;
+    }
+
+    // Геттеры и сеттеры
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public int getDuration() { return duration; }
+    public void setDuration(int duration) { this.duration = duration; }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Node node = (Node) obj;
+        return id.equals(node.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
